0a2d211ff57980a7b05e5ab7d18d35eeaa7fe4a3
sm0bin <shehjad0mobin@gmail.com>
2024-04-07 23:24:48 +0600

    nav paths

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,10 @@ import ErrorPage from "./components/pages/ErrorPage";
 import Home from "./components/pages/Home";
 import { Toaster } from "react-hot-toast";
 import ProfileForm from "./components/pages/ProfileForm";
+import Profile from "./components/pages/Profile";
+import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
+
 
 const router = createBrowserRouter([
   {
@@ -22,7 +26,23 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
         path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/profile/new",
         element: <ProfileForm />,
       }
     ],

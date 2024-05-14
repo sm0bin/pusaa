24cc77370b6a4b461d30b16680ee0c4bfb75afa5
sm0bin <shehjad0mobin@gmail.com>
2024-05-14 16:40:11 +0600

    route

--- src/main.jsx
+++ src/main.jsx
@@ -1,89 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import ErrorPage from "./components/pages/ErrorPage";
-import Home from "./components/pages/Home";
+import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import CreateProfile from "./components/pages/CreateProfile";
-import Profile from "./components/pages/Profile";
-import Login from "./components/pages/auth/Login";
-import SignUp from "./components/pages/auth/SignUp";
-import ForgotPassword from "./components/pages/ForgotPassword";
-import ResetPassword from "./components/pages/ResetPassword";
-import EditProfile from "./components/pages/EditProfile";
-import Members from "./components/pages/Members";
-import Gallery from "./components/pages/Gallery";
-import AboutUs from "./components/pages/AboutUs";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import "./index.css";
 import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { router } from "./routes/Routes";
 const queryClient = new QueryClient();
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <SignUp />,
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/profile/new",
-        element: <CreateProfile />,
-      },
-      {
-        path: "/profile/edit",
-        element: <EditProfile />,
-      },
-      {
-        path: "/members",
-        element: <Members />,
-      },
-      {
-        path: "/gallery",
-        element: <Gallery />,
-      },
-      {
-        path: "/about-us",
-        element: <AboutUs />,
-      },
-      // {
-      //   path: "/notice-and-events",
-      //   element: <NoticeAndEvents />,
-      // },
-      {
-        path: "/forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "/reset-password/:token",
-        element: <ResetPassword />,
-      }
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
